87ccd38a36c7015e77428bf52e13797128d3566d
Anand Babu <babu.anand@pearson.com>
2020-03-27 16:23:07 +0530

    season logic

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,18 +1,32 @@
 import React from "react";
 
+const seasonConfig = {
+    summer: {
+        'text': "Lets hit the beach",
+        'iconName': "sun"
+    },
+    winter: {
+        'text': "Burr, it is chilly",
+        'iconName': "snowFlakes"
+    }
+};
+
 const getSeason = (lat, month) => {
- if (month >= 2 && month <= 9){
-     return lat > 0 ? 'summer' : 'winter';
- }else {
-     return lat > 0 ? 'winter' : 'summer';
- }
+    if (month >= 2 && month <= 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
 };
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season];
     return (
         <div>
-            {season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'}
+            <i className={`${iconName} icon`}/>
+            <h1>{text}</h1>
+            <i className={`${iconName} icon`}/>
         </div>
     );
 };
